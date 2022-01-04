5120dbfa9142be2afd0b4b91d7e3b2205a8fa20d
hailiting <hailiting@yeah.net>
2022-01-04 18:00:01 +0800

    re back

--- src/views/Pool/index.tsx
+++ src/views/Pool/index.tsx
@@ -5,7 +5,6 @@ import { Text, Flex, CardBody, CardFooter, Button, AddIcon } from '@kaco/uikit';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'contexts/Localization';
 import useActiveWeb3React from 'hooks/useActiveWeb3React';
-import { useFarms, usePollFarmsData } from 'state/farms/hooks';
 import FullPositionCard from '../../components/PositionCard';
 import { useTokenBalancesWithLoadingIndicator } from '../../state/wallet/hooks';
 import { usePairs } from '../../hooks/usePairs';
@@ -16,11 +15,6 @@ import Page from '../Page';
 import Status from './Status';
 
 const Body = styled(CardBody)``;
-const FullPositionCardWrap = styled.div`
-  max-height: 300px;
-  overflow-y: auto;
-  margin-bottom: 20px;
-`;
 
 export default function Pool() {
   const { account } = useActiveWeb3React();
@@ -36,57 +30,27 @@ export default function Pool() {
     () => tokenPairsWithLiquidityTokens.map((tpwlt) => tpwlt.liquidityToken),
     [tokenPairsWithLiquidityTokens],
   );
-  const [, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(account ?? undefined, liquidityTokens);
-  const { data: farmsLP } = useFarms();
-  usePollFarmsData();
-  const farmsLPPlus = useMemo(
-    () =>
-      farmsLP.filter((v) => {
-        if (Number((v?.multiplier ?? '0').match(/\d+/g).join('')) > 0) {
-          return true;
-        } else {
-          return false;
-        }
-      }),
-    [farmsLP],
+  const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
+    account ?? undefined,
+    liquidityTokens,
   );
+
   // fetch the reserves for all V2 pools in which the user has a balance
-  // const liquidityTokensWithBalances = useMemo(
-  //   () =>
-  //     tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
-  //       v2PairsBalances[liquidityToken.address]?.greaterThan('0'),
-  //     ),
-  //   [tokenPairsWithLiquidityTokens, v2PairsBalances],
-  // );
-  const liquidityTokensWithBalances = tokenPairsWithLiquidityTokens;
+  const liquidityTokensWithBalances = useMemo(
+    () =>
+      tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
+        v2PairsBalances[liquidityToken.address]?.greaterThan('0'),
+      ),
+    [tokenPairsWithLiquidityTokens, v2PairsBalances],
+  );
 
   const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens));
   const v2IsLoading =
     fetchingV2PairBalances ||
     v2Pairs?.length < liquidityTokensWithBalances.length ||
-    v2Pairs?.some((V2Pair) => !V2Pair) ||
-    !farmsLPPlus.length;
+    v2Pairs?.some((V2Pair) => !V2Pair);
 
-  // const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair));
-  const _allV2PairsWithLiquidity = useMemo(
-    () => v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair)),
-    [v2Pairs],
-  );
-  const allV2PairsWithLiquidity = useMemo(() => {
-    return _allV2PairsWithLiquidity.filter((v: Pair) => {
-      return farmsLPPlus.filter((item) => {
-        const _lpSymbol =
-          item.lpSymbol.indexOf('SDN') > -1 && item.lpSymbol.indexOf('WSDN') === -1
-            ? item.lpSymbol.replace('SDN', 'WSDN')
-            : item.lpSymbol;
-        const _symbol01 = v.token0.symbol.indexOf('JPCY') > -1 ? 'JPYC' : v.token0.symbol;
-        const _symbol02 = v.token1.symbol.indexOf('JPCY') > -1 ? 'JPYC' : v.token1.symbol;
-        return (
-          _lpSymbol.indexOf(`${_symbol01}-${_symbol02}`) > -1 || _lpSymbol.indexOf(`${_symbol02}-${_symbol01}`) > -1
-        );
-      }).length;
-    });
-  }, [farmsLPPlus, _allV2PairsWithLiquidity]);
+  const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair));
 
   const renderBody = () => {
     if (!account) {
@@ -100,17 +64,13 @@ export default function Pool() {
       );
     }
     if (allV2PairsWithLiquidity?.length > 0) {
-      return (
-        <FullPositionCardWrap>
-          {allV2PairsWithLiquidity.map((v2Pair, index) => (
-            <FullPositionCard
-              key={v2Pair.liquidityToken.address}
-              pair={v2Pair}
-              mb={index < allV2PairsWithLiquidity.length - 1 ? '16px' : 0}
-            />
-          ))}
-        </FullPositionCardWrap>
-      );
+      return allV2PairsWithLiquidity.map((v2Pair, index) => (
+        <FullPositionCard
+          key={v2Pair.liquidityToken.address}
+          pair={v2Pair}
+          mb={index < allV2PairsWithLiquidity.length - 1 ? '16px' : 0}
+        />
+      ));
     }
     return <Status span={t('No liquidity found.')} status="no-liquidity" />;
   };
