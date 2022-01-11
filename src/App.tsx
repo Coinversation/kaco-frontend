1972ee63580e5deac838a9ce57d97abae5294125
hailiting <hailiting@yeah.net>
2022-01-11 11:38:27 +0800

    Important: delete unnecessary business logic

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ import PageLoader from './components/Loader/PageLoader';
 import EasterEgg from './components/EasterEgg';
 import history from './routerHistory';
 // Views included in the main bundle
-import Pools from './views/Pools';
-import Swap from './views/Swap';
+
 import {
   RedirectDuplicateTokenIds,
   RedirectOldAddLiquidityPathStructure,
@@ -29,19 +28,9 @@ import { usePollCoreFarmData } from './state/farms/hooks';
 // Only pool is included in the main bundle because of it's the most visited page
 const Home = lazy(() => import('./views/Home'));
 const Farms = lazy(() => import('./views/Farms'));
-// const FarmAuction = lazy(() => import('./views/FarmAuction'));
-// const Lottery = lazy(() => import('./views/Lottery'));
-// const Ifos = lazy(() => import('./views/Ifos'));
+const Pools = lazy(() => import('./views/Pools'));
+const Swap = lazy(() => import('./views/Swap'));
 const NotFound = lazy(() => import('./views/NotFound'));
-// const Collectibles = lazy(() => import('./views/Collectibles'));
-// const Teams = lazy(() => import('./views/Teams'));
-// const Team = lazy(() => import('./views/Teams/Team'));
-// const Profile = lazy(() => import('./views/Profile'));
-// const TradingCompetition = lazy(() => import('./views/TradingCompetition'));
-// const Predictions = lazy(() => import('./views/Predictions'));
-// const Voting = lazy(() => import('./views/Voting'));
-// const Proposal = lazy(() => import('./views/Voting/Proposal'));
-// const CreateProposal = lazy(() => import('./views/Voting/CreateProposal'));
 const AddLiquidity = lazy(() => import('./views/AddLiquidity'));
 const Liquidity = lazy(() => import('./views/Pool'));
 const PoolFinder = lazy(() => import('./views/PoolFinder'));
@@ -73,24 +62,12 @@ const App: React.FC = () => {
             <Route path="/" exact>
               <Home />
             </Route>
-            {/* <Route exact path="/farms/auction">
-              <FarmAuction />
-            </Route> */}
             <Route path="/farms">
               <Farms />
             </Route>
             <Route path="/pools">
               <Pools />
             </Route>
-            {/* <Route path="/lottery">
-              <Lottery />
-            </Route> */}
-            {/* <Route path="/ifo">
-              <Ifos />
-            </Route> */}
-            {/* <Route path="/collectibles">
-              <Collectibles />
-            </Route> */}
             <Route exact path="/nft/pools">
               <NftPools />
             </Route>
@@ -103,31 +80,6 @@ const App: React.FC = () => {
             <Route path="/nft/wallet/mint">
               <NftMint />
             </Route>
-            {/* <Route exact path="/teams">
-              <Teams />
-            </Route>
-            <Route path="/teams/:id">
-              <Team />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-            <Route path="/competition">
-              <TradingCompetition />
-            </Route>
-            <Route path="/prediction">
-              <Predictions />
-            </Route>
-            <Route exact path="/voting">
-              <Voting />
-            </Route>
-            <Route exact path="/voting/proposal/create">
-              <CreateProposal />
-            </Route>
-            <Route path="/voting/proposal/:id">
-              <Proposal />
-            </Route> */}
-
             {/* Using this format because these components use routes injected props. We need to rework them with hooks */}
             <Route exact strict path="/swap" component={Swap} />
             <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
@@ -157,10 +109,6 @@ const App: React.FC = () => {
             <Route path="/syrup">
               <Redirect to="/pools" />
             </Route>
-            {/* <Route path="/nft">
-              <Redirect to="/collectibles" />
-            </Route> */}
-
             {/* 404 */}
             <Route component={NotFound} />
           </Switch>
--- src/views/Profile/components/NextStepButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ArrowForwardIcon, Button, ButtonProps } from '@kaco/uikitv2';
-
-const NextStepButton: React.FC<ButtonProps> = (props) => {
-  return <Button endIcon={<ArrowForwardIcon color="currentColor" />} {...props} />;
-};
-
-export default NextStepButton;
