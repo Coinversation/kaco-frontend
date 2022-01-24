b4abaf2bebf76e0f55ded33e19b3d75b7edc9fe0
hailiting <hailiting@yeah.net>
2022-01-24 11:28:00 +0800

    delete console

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -1,5 +1,5 @@
 import { parseUnits } from '@ethersproject/units';
-import { Currency, CurrencyAmount, ETHER, JSBI, Token, TokenAmount, Trade } from '@kaco/sdk';
+import { Currency, CurrencyAmount, JSBI, Token, TokenAmount, Trade } from '@kaco/sdk';
 import { ParsedQs } from 'qs';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
