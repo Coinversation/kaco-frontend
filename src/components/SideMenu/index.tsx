1457734b0c0ef10d3d2685e05a51f8e4b5550da9
hailiting <hailiting@yeah.net>
2022-02-21 10:13:41 +0800

    remove unnecessary svg

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -19,7 +19,7 @@ import TradeSvg from '../svg/Trade';
 import FarmSvg from '../svg/Farm';
 // import NftSvg from '../svg/Nft';
 import HomeSvg from '../svg/Home';
-import PoolsSvg from '../svg/PoolsSvg';
+// import PoolsSvg from '../svg/PoolsSvg';
 // import InfoSvg from '../svg/InfoSvg';
 import LogoSvg from './imgs/icon_logo.svg';
 import Header from './Header';
